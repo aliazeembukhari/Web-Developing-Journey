e891ec2f34e7abce590c03db9584e5aadc04faa9
aliazeembukhari <aliazeembukhari93@gmail.com>
2026-04-03 21:48:43 +0500

    Java Script
    Here are the some JavaScript Programs

---
+++ Java Script/bot_introduction.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName , botLocation , favoriteLanguage;
+botName="bot";
+botLocation = "Pak";
+favoriteLanguage="C++";
+console.log("My name is "+botName+ " and I live on "+botLocation+".");
+console.log("My favorite programming language is "+ favoriteLanguage+".");
+let codingFact;
+codingFact="The language created after C was " +favoriteLanguage;
+console.log(codingFact);
+codingFact="The language similar to C lan is  " +favoriteLanguage;
+console.log(codingFact);
+codingFact="The Language which can be use in app dev data or in game dev is " +favoriteLanguage;
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName +" from "+botLocation+".");
